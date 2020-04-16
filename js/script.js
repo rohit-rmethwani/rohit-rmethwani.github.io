ae5744b30fe5088efc821031c4e341a257c78f75
Rohit Methwani <rohit.methwan31@gmail.com>
2020-04-17 00:42:29 +0530

    Added sidebar with responsivenss. Version 1 Done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 $(function () {
+	$("#hamburger").click(function(){
+		$("#open-nav").addClass("open-nav-active");
+	});
+
+	$("#close-navbar").click(function(){
+		$("#open-nav").removeClass("open-nav-active");
+	});
+
 	$(document).mousemove(function(event){
 		$("#mouse-trail").css({"top":event.pageY+"px", "left":event.pageX+"px"});
-	  });
-	  new WOW().init();
-	$('.counter').counterUp({
+	});
+	
+	$("#navigation-links").click(function(event){
+		console.log(event.target);
+		if(event.target.classList.contains("navigation-link")){
+			console.log("Inside");
+			$("#open-nav").removeClass("open-nav-active");
+		}
+	});
+
+  	new WOW().init();
+	
+	  $('.counter').counterUp({
 		delay: 10,
 		time: 1500,
 	});
+	
 });
