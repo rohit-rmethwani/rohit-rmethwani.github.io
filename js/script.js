8fa6761abe399019b06b72225d430ac74884bb22
Rohit Methwani <rohit.methwan31@gmail.com>
2020-04-16 21:33:15 +0530

    Added animations

--- js/script.js
+++ js/script.js
@@ -2,4 +2,9 @@ $(function () {
 	$(document).mousemove(function(event){
 		$("#mouse-trail").css({"top":event.pageY+"px", "left":event.pageX+"px"});
 	  });
+	  new WOW().init();
+	$('.counter').counterUp({
+		delay: 10,
+		time: 1500,
+	});
 });
