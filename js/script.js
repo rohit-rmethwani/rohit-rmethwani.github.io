b10ca2f49c6bf2d70341f6b5d7152814b011d086
Rohit Methwani <rohit.methwan31@gmail.com>
2020-09-26 17:45:59 +0530

    Fixed all bugs, linkings

--- js/script.js
+++ js/script.js
@@ -23,46 +23,6 @@ $(function () {
 		}
 	});
 
-	$("#hamburger").click(function(){
-		$("#open-nav").addClass("open-nav-active");
-	});
-
-	$("#close-navbar").click(function(){
-		$("#open-nav").removeClass("open-nav-active");
-	});
-
-	$(document).mousemove(function(event){
-		$("#mouse-trail").css({"top":event.pageY+"px", "left":event.pageX+"px"});
-	});
-	
-	$("#navigation-links").click(function(event){
-		console.log(event.target);
-		if(event.target.classList.contains("navigation-link")){
-			console.log("Inside");
-			$("#open-nav").removeClass("open-nav-active");
-		}
-	});
-
-  	new WOW().init();
-	
-	  $('.counter').counterUp({
-		delay: 10,
-		time: 1500,
-	});
-
-	if ($('.text-slider').length == 1) {
-		var typed_strings = $('.text-slider-items').text();
-			var typed = new Typed('.text-slider', {
-				strings: typed_strings.split(','),
-				typeSpeed: 120,
-				loop: true,
-				backDelay: 1500,
-				backSpeed: 30,
-				cursorChar: "|"
-			});
-		}
-	
-	
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:1955155,hjsv:6};
